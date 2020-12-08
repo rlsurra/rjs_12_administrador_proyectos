4a0ff8f26fdea967cef8e6b0de393a6da2991018
rlsurra <rlsurra@hotmail.com>
2020-12-08 13:08:42 -0300

    Estructura basica + Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Registro from './components/auth/Registro';
+import Proyectos from './components/proyectos/Proyectos';
 
 function App() {
   return (
-    <p> Hola </p>
+    <Router>
+      {/* Lo que se ve en todas las paginas */}
+      <Switch>
+        {/* Lo que va cambiando en la pagina */}
+        <Route exact path="/" component={Login} />
+        <Route exact path="/registro" component={Registro} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
   );
 }
 
