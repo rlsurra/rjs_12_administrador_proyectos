49516822fccb376cf0961c03dbae987f8cd068a8
rlsurra <rlsurra@hotmail.com>
2021-01-06 17:52:30 -0300

    Conexion a mongodb

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,21 @@
-console.log('Desde index.js');
+const express = require('express');
+const conectarDB = require('./config/db');
+
+//Creacion del servidor
+const app = express();
+
+//Conectar a la base de datos
+conectarDB();
+
+//Puerto de la app
+const PORT = process.env.PORT || 4000
+
+//Definir la página principal
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+})
+
+//Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
